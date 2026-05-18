85ac60ee69561f3d453f77e9fcdd2af56e6e2078
Fideligo <mrafighalibf@gmail.com>
2026-05-18 15:34:20 +0700

    Experiment 2.1: Original code, and how it run

---
+++ src/bin/client.rs
@@ -0,0 +1,43 @@
+use futures_util::stream::StreamExt;
+use futures_util::SinkExt;
+use http::Uri;
+use tokio::io::{AsyncBufReadExt, BufReader};
+use tokio_websockets::{ClientBuilder, Message};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let (mut ws_stream, _) =
+        ClientBuilder::from_uri(Uri::from_static("ws://127.0.0.1:2000"))
+            .connect()
+            .await?;
+
+    let stdin = tokio::io::stdin();
+    let mut stdin = BufReader::new(stdin).lines();
+
+    println!("Fide's Komputer - From server: Welcome to chat! Type a message");
+
+    loop {
+        tokio::select! {
+            incoming = ws_stream.next() => {
+                match incoming {
+                    Some(Ok(msg)) => {
+                        if msg.is_text() {
+                            println!("Fide's Komputer - From server: {}", msg.as_text().unwrap());
+                        }
+                    }
+                    Some(Err(err)) => return Err(err.into()),
+                    None => return Ok(()),
+                }
+            }
+            line = stdin.next_line() => {
+                match line {
+                    Ok(Some(text)) => {
+                        ws_stream.send(Message::text(text)).await?;
+                    }
+                    Ok(None) => return Ok(()),
+                    Err(err) => return Err(err.into()),
+                }
+            }
+        }
+    }
+}
---
+++ src/bin/server.rs
@@ -0,0 +1,57 @@
+use futures_util::sink::SinkExt;
+use futures_util::stream::StreamExt;
+use std::error::Error;
+use std::net::SocketAddr;
+use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::broadcast::{channel, Sender};
+use tokio_websockets::{Message, ServerBuilder, WebsocketStream};
+
+async fn handle_connection(
+    addr: SocketAddr,
+    mut ws_stream: WebsocketStream<TcpStream>,
+    bcast_tx: Sender<String>,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    println!("New connection from Fide's Komputer {}", addr);
+    let mut bcast_rx = bcast_tx.subscribe();
+
+    loop {
+        tokio::select! {
+            incoming = ws_stream.next() => {
+                match incoming {
+                    Some(Ok(msg)) => {
+                        if msg.is_text() {
+                            let text = msg.as_text().unwrap();
+                            println!("From client {}: {}", addr, text);
+                            bcast_tx.send(text.into())?;
+                        }
+                    }
+                    Some(Err(err)) => return Err(err.into()),
+                    None => return Ok(()),
+                }
+            }
+            msg = bcast_rx.recv() => {
+                let msg = msg?;
+                ws_stream.send(Message::text(msg)).await?;
+            }
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
+    let (bcast_tx, _) = channel(16);
+    let listener = TcpListener::bind("127.0.0.1:2000").await?;
+    println!("listening on port 2000");
+
+    loop {
+        let (socket, addr) = listener.accept().await?;
+        let bcast_tx = bcast_tx.clone();
+        tokio::spawn(async move {
+            let ws_stream = ServerBuilder::new().accept(socket).await?;
+            if let Err(e) = handle_connection(addr, ws_stream, bcast_tx).await {
+                println!("Error processing connection: {}", e);
+            }
+            Ok::<_, Box<dyn Error + Send + Sync>>(())
+        });
+    }
+}
